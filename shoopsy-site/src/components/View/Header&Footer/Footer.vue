b20a6589421785b4784383ac7360179dbf4a91e4
skeynikhilanakhiya <skey.anakhiyanikhil@gmail.com>
2023-05-24 18:17:37 +0530

    Background Color Change

--- shoopsy-site/src/components/View/Header&Footer/Footer.vue
+++ shoopsy-site/src/components/View/Header&Footer/Footer.vue
@@ -1,16 +1,16 @@
 <template >
 
-    <footer class="text-center  bg-dark " >
+    <footer class="text-center  bg-light   " >
       <!-- Section: Social media -->
       <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         <!-- Left -->
-        <div class="me-5 d-none d-lg-block" style="color: aliceblue;">
+        <div class="me-5 d-none d-lg-block" style="color: rgb(17, 18, 18);">
           <span>Get connected with us on social networks:</span>
         </div>
         <!-- Left -->
 
         <!-- Right -->
-        <div style="color: aliceblue;">
+        <div style="color: rgb(20, 21, 22);">
           <a href="" class="me-4 text-reset">
             <i class="fab fa-facebook-f"></i>
           </a>
@@ -35,7 +35,7 @@
       <!-- Section: Social media -->
 
       <!-- Section: Links  -->
-      <section class="" style="color: aliceblue;">
+      <section class="" style="color: rgb(17, 18, 18);">
         <div class="container text-center text-md-start mt-5">
           <!-- Grid row -->
           <div class="row mt-3">
@@ -43,7 +43,7 @@
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               <!-- Content -->
               <h6 class="text-uppercase fw-bold mb-4">
-                <i class="fas fa-gem me-3"></i>Company name
+                <i class="fas fa-gem me-3"></i>Shoppy-fy.com
               </h6>
               <p>
                 Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -56,19 +56,19 @@
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               <!-- Links -->
               <h6 class="text-uppercase fw-bold mb-4">
-                Products
+                Online Shopping
               </h6>
               <p>
-                <a href="#!" class="text-reset">Angular</a>
+                <a href="#!" class="text-reset">Men</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">React</a>
+                <a href="#!" class="text-reset">Women</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Vue</a>
+                <a href="#!" class="text-reset">Kids</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Laravel</a>
+                <a href="#!" class="text-reset">Home&Living</a>
               </p>
             </div>
             <!-- Grid column -->
@@ -101,7 +101,7 @@
               <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
               <p>
                 <i class="fas fa-envelope me-3"></i>
-                info@example.com
+                shoopy-fy.com
               </p>
               <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
               <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
@@ -115,8 +115,8 @@
 
       <!-- Copyright -->
       <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);color:aliceblue" >
-        © 2021 Copyright:
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        © 2023 Copyright:
+        <a class="text-reset fw-bold" href="">shoopy-fy.com</a>
       </div>
       <!-- Copyright -->
     </footer>
